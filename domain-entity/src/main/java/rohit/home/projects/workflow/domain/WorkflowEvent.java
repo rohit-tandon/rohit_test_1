937e43d922bb9a6e4f5c1dd793d4735205f25101
rohit tandon <>
2016-08-16 03:30:00 +0200

    working set 1

--- domain-entity/src/main/java/rohit/home/projects/workflow/domain/WorkflowEvent.java
+++ domain-entity/src/main/java/rohit/home/projects/workflow/domain/WorkflowEvent.java
@@ -1,8 +1,13 @@
 package rohit.home.projects.workflow.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
+@Entity(name="WORKFLOWEVENT")
 public class WorkflowEvent {
 	
 	@Id @GeneratedValue
--- services-war/src/main/java/rohit/home/projects/workflow/controller/WorkflowController.java
+++ services-war/src/main/java/rohit/home/projects/workflow/controller/WorkflowController.java
@@ -4,20 +4,33 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rohit.home.projects.workflow.domain.Workflow;
+import rohit.home.projects.workflow.domain.WorkflowEvent;
+import rohit.home.projects.workflow.domain.repository.WorkflowEventRepository;
 import rohit.home.projects.workflow.domain.repository.WorkflowRepository;
 
 @RestController
 public class WorkflowController {
-	
+
 	@Autowired
 	WorkflowRepository workflowRepository;
-	
-	@RequestMapping("/workflow//list")
+	@Autowired
+	WorkflowEventRepository workflowEventRepository;
+
+	@RequestMapping(name="/workflow/list")
+	@ResponseBody
 	public List<Workflow> getAllWorkflows() {
 		return workflowRepository.findAll();
+	}
+	
+	@RequestMapping(name="/workflowEvents/list", method=RequestMethod.GET)
+	@ResponseBody
+	public List<WorkflowEvent> getAllWorkflowEvents() {
+		return workflowEventRepository.findAll();
 	}
 	
 }
